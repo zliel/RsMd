6808167b196ab19476bffbe87197f0daaa909734
zliel <47030040+zliel@users.noreply.github.com>
2025-06-05 17:17:00 -0700

    Added a function for reading files to strings

--- src/io.rs
+++ src/io.rs
@@ -1,1 +1,11 @@
+use std::{fs::File, io::Read};
 
+pub fn read_file(file_path: &str) -> String {
+    let mut md_file: File = File::open(file_path).expect("Couldn't open file");
+    let mut contents = String::new();
+    md_file
+        .read_to_string(&mut contents)
+        .expect("Couldn't read file into string");
+
+    contents
+}
