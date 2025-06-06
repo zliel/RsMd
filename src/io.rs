1c38f561b1492f9d11162fd3f3ce9bd6eff8a643
zliel <47030040+zliel@users.noreply.github.com>
2025-06-05 17:32:21 -0700

    Added a better error message

--- src/io.rs
+++ src/io.rs
@@ -1,7 +1,9 @@
 use std::{fs::File, io::Read};
 
 pub fn read_file(file_path: &str) -> String {
-    let mut md_file: File = File::open(file_path).expect("Couldn't open file");
+    let mut md_file: File =
+        File::open(file_path).unwrap_or_else(|_| panic!("Couldn't open file: \"{file_path}\""));
+
     let mut contents = String::new();
     md_file
         .read_to_string(&mut contents)
