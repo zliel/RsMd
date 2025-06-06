d17090217a20ae20854172134856d139c999af97
zliel <47030040+zliel@users.noreply.github.com>
2025-06-05 17:50:35 -0700

    Added error handling for args

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,11 @@ use std::env::args;
 fn main() {
     println!("Hello, world!");
     let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        eprintln!("Error: Missing file path argument.");
+        eprintln!("Usage: cargo run <file_path>");
+        std::process::exit(1);
+    }
+
     let file_path = &args[1];
 }
