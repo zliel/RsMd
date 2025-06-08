725dac401b4a563be7dde01b7dec8beb76a33527
zliel <47030040+zliel@users.noreply.github.com>
2025-06-08 01:08:38 -0700

    Added functionality for parsing text

--- src/parser.rs
+++ src/parser.rs
@@ -73,8 +73,36 @@ impl TokenCursor {
 pub fn parse_inline(markdown_tokens: Vec<Token>) -> Vec<MdInlineElement> {
     let mut parsed_inline_elements: Vec<MdInlineElement> = Vec::new();
 
+    let mut cursor: TokenCursor = TokenCursor {
+        tokens: markdown_tokens,
+        current_position: 0,
+    };
+
+    let mut buffer: String = String::new();
+
+    let mut current_token: &Token;
+    while !cursor.is_at_eof() {
+        current_token = cursor.current().expect("Token should be valid markdown");
+
+        match current_token {
+            Token::Text(string) => buffer.push_str(string),
+            _ => push_buffer_to_elements(&mut parsed_inline_elements, &mut buffer),
+        }
+        cursor.advance();
+    }
+
+    push_buffer_to_elements(&mut parsed_inline_elements, &mut buffer);
 
     parsed_inline_elements
+}
+
+fn push_buffer_to_elements(elements: &mut Vec<MdInlineElement>, buffer: &mut String) {
+    if !&buffer.is_empty() {
+        elements.push(MdInlineElement::Text {
+            content: buffer.to_string(),
+        });
+        buffer.clear();
+    }
 }
 
 #[cfg(test)]
