122318c6d96f8049a29c398ec0da7cad150e7974
zliel <47030040+zliel@users.noreply.github.com>
2025-06-08 14:08:57 -0700

    Added bold text to the parser

--- src/parser.rs
+++ src/parser.rs
@@ -85,6 +85,11 @@ pub fn parse_inline(markdown_tokens: Vec<Token>) -> Vec<MdInlineElement> {
         current_token = cursor.current().expect("Token should be valid markdown");
 
         match current_token {
+            Token::DoubleAsterisk => {
+                parsed_inline_elements.push(MdInlineElement::Bold {
+                    content: parse_bold(&mut cursor),
+                });
+            }
             Token::Escape(esc_char) => buffer.push_str(format!("\\{esc_char}").as_str()),
             Token::Text(string) => buffer.push_str(string),
             Token::Whitespace => buffer.push(' '),
@@ -98,6 +103,21 @@ pub fn parse_inline(markdown_tokens: Vec<Token>) -> Vec<MdInlineElement> {
     parsed_inline_elements
 }
 
+fn parse_bold(cursor: &mut TokenCursor) -> Vec<MdInlineElement> {
+    cursor.advance();
+
+    let mut inner_tokens: Vec<Token> = Vec::new();
+    while let Some(token) = cursor.current() {
+        if token == &Token::DoubleAsterisk {
+            break;
+        }
+
+        inner_tokens.push(token.clone());
+        cursor.advance();
+    }
+
+    parse_inline(inner_tokens)
+}
 fn push_buffer_to_elements(elements: &mut Vec<MdInlineElement>, buffer: &mut String) {
     if !&buffer.is_empty() {
         elements.push(MdInlineElement::Text {
