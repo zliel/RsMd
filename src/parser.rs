7e80bf6037eba10e75cdab4bb20d18904cb4e564
zliel <47030040+zliel@users.noreply.github.com>
2025-06-07 20:51:41 -0700

    Added a struct for a cursor over the tokens

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use crate::lexer::Token;
+
 #[derive(Debug, PartialEq)]
 pub enum MdBlockElement {
     Header {
@@ -38,6 +40,42 @@ pub enum MdInlineElement {
     },
 }
 
+#[derive(Debug)]
+pub struct TokenCursor {
+    tokens: Vec<Token>,
+    current_position: usize,
+}
+
+impl TokenCursor {
+    fn current(&self) -> Option<&Token> {
+        self.tokens.get(self.current_position)
+    }
+
+    fn peek_ahead(&self, n: usize) -> Option<&Token> {
+        self.tokens.get(self.current_position + n)
+    }
+
+    fn advance(&mut self) {
+        if self.current_position < self.tokens.len() {
+            self.current_position += 1;
+        }
+    }
+
+    fn position(&self) -> usize {
+        self.current_position
+    }
+
+    fn is_at_eof(&self) -> bool {
+        self.current_position > self.tokens.len()
+    }
+}
+
+pub fn parse_inline(markdown_tokens: Vec<Token>) -> Vec<MdInlineElement> {
+    let mut parsed_inline_elements: Vec<MdInlineElement> = Vec::new();
+
+
+    parsed_inline_elements
+}
 
 #[cfg(test)]
 mod test;
