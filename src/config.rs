0af5852746c391b31c7e34243e7decae14a610bb
zliel <47030040+zliel@users.noreply.github.com>
2025-07-04 15:15:43 -0700

    Updated `config.rs` to use serde Deserialization

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,14 @@
 //! This module handles the configuration I/O for the application.
-use toml::Table;
+use serde::Deserialize;
 
 /// Represents the global configuration for the application.
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct Config {
     pub lexer: LexerConfig,
 }
 
-#[derive(Debug)]
+/// Managers all configuration for tokenization
+#[derive(Debug, Deserialize)]
 pub struct LexerConfig {
     pub tab_size: usize,
 }
@@ -24,22 +25,9 @@ impl Config {
         let contents = std::fs::read_to_string(file_path)
             .map_err(|e| format!("Failed to read config file: {}", e))?;
 
-        let config: Table =
+        let config: Config =
             toml::from_str(&contents).map_err(|e| format!("Failed to parse config file: {}", e))?;
 
-        let lexer_table = config
-            .get("lexer")
-            .and_then(|val| val.as_table())
-            .ok_or("Missing `[lexer]` section heading in config")?;
-
-        let tab_size: usize = lexer_table
-            .get("tab_size")
-            .and_then(|val| val.as_integer())
-            .map(|val| val as usize)
-            .ok_or("Missing or invalid 'tab_size' in config")?;
-
-        Ok(Config {
-            lexer: LexerConfig { tab_size },
-        })
+        Ok(config)
     }
 }
