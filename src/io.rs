ee7f179228b1d94dd695195913db65b63f41c46d
zliel <47030040+zliel@users.noreply.github.com>
2025-07-07 15:56:47 -0700

    Added a function for writing html to files

--- src/io.rs
+++ src/io.rs
@@ -1,6 +1,10 @@
 //! This module provides functionality to related to reading/writing files.
 
-use std::{error::Error, fs::File, io::Read};
+use std::{
+    error::Error,
+    fs::{File, create_dir_all},
+    io::{Read, Write},
+};
 
 /// Reads the contents of a file into a String.
 ///
@@ -20,4 +24,34 @@ pub fn read_file(file_path: &str) -> Result<String, Box<dyn Error>> {
         .map_err(|e| format!("Failed to read file '{}': {}", file_path, e))?;
 
     Ok(contents)
+}
+
+pub fn write_html_to_file(
+    html: &str,
+    output_dir: &str,
+    input_filename: &str,
+) -> Result<(), Box<dyn Error>> {
+    println!("Writing output to directory: {}", output_dir);
+
+    // Ensure the output directory exists
+    create_dir_all(output_dir)
+        .map_err(|e| format!("Failed to create directory '{}': {}", output_dir, e))?;
+
+    // Get only the filename without the extension and without the path
+    let input_filename = input_filename
+        .rsplit('/')
+        .next()
+        .ok_or("Failed to extract filename from input path")?
+        .trim_end_matches(".md");
+
+    let output_file_path = format!("{}/{}.html", output_dir, input_filename);
+    let mut output_file = File::create(&output_file_path)
+        .map_err(|e| format!("Failed to create file '{}': {}", output_file_path, e))?;
+
+    output_file
+        .write_all(html.as_bytes())
+        .map_err(|e| format!("Failed to write to file '{}': {}", output_file_path, e))?;
+
+    println!("HTML written to: {}", output_file_path);
+    Ok(())
 }
