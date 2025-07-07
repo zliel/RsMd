f4b0c66cb5ed3f89853cc40a586a97ae90d5c615
zliel <47030040+zliel@users.noreply.github.com>
2025-07-07 16:41:56 -0700

    Updated the html generator to add a link to the css file

--- src/html_generator.rs
+++ src/html_generator.rs
@@ -1,17 +1,31 @@
+use crate::CONFIG;
 use crate::types::{MdBlockElement, ToHtml};
 
 pub fn generate_html(md_elements: Vec<MdBlockElement>) -> String {
     let mut html_output = String::new();
 
     // Build the HTML head
-    let head = r#"<!DOCTYPE html>
+    let mut head = String::from(
+        r#"<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>Markdown Document</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    "#;
+    "#,
+    );
+
+    let css_file = CONFIG.get().unwrap().html.css_file.clone();
+    if css_file == "default" {
+        head.push_str("<link rel=\"stylesheet\" href=\"styles.css\">\n");
+    } else {
+        head.push_str(&format!(
+            "<link rel=\"stylesheet\" href=\"{}\">\n",
+            css_file
+        ));
+    }
+
+    head.push_str("</head>\n");
 
     let mut body = String::from("<body>\n");
     let inner_html: String = md_elements
@@ -23,7 +37,7 @@ pub fn generate_html(md_elements: Vec<MdBlockElement>) -> String {
     body.push_str(&inner_html);
     body.push_str("\n</body>\n");
 
-    html_output.push_str(head);
+    html_output.push_str(&head);
     html_output.push_str(&body);
     html_output.push_str("</html>\n");
 
