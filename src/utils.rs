7f69b9591d714b0c55f7771d1e8bae533a0accc3
zliel <47030040+zliel@users.noreply.github.com>
2025-07-13 15:24:24 -0700

    Added a helper function for building relative path prefixes

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::path::{Path, PathBuf};
+
 /// Utility function for pushing a String buffer to a generic collection.
 ///
 /// This function checks if the buffer is not empty, converts it to the type `T`, and pushes it to
@@ -18,4 +20,14 @@ where
         collection.push(T::from(buffer.to_string()));
         buffer.clear();
     }
+}
+
+pub fn build_rel_prefix(html_rel_path: &str) -> PathBuf {
+    let rel_path = Path::new(html_rel_path);
+    let depth = rel_path.parent().map_or(0, |p| p.components().count());
+    let mut rel_prefix = PathBuf::new();
+    for _ in 0..depth {
+        rel_prefix.push("..");
+    }
+    rel_prefix
 }
